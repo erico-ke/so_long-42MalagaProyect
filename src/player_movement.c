e3ffbe2af72f2f93b616f49cce11317b0b08da39
erico-ke <eliasricokergaravat@gmail.com>
2025-03-03 18:14:48 +0100

    a

--- src/player_movement.c
+++ src/player_movement.c
@@ -6,7 +6,7 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 17:16:31 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/03/03 16:55:12 by erico-ke         ###   ########.fr       */
+/*   Updated: 2025/03/03 18:13:56 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ static void	move_p_y(t_map *map, int y, int x, char dir)
 	if (dir == 'w' && map->map[y - 1][x] != '1')
 	{
 		if (map->map[y - 1][x] == 'C')
+		{
 			map->coin_c -= 1;
+			map->map[y - 1][x] = '0';
+		}
 		if (map->map[y - 1][x] == 'E' && map->coin_c == 0)
 			mlx_close_window(map->wind);
 		a_move_map_charge(map, y - 1, x);
@@ -26,7 +29,10 @@ static void	move_p_y(t_map *map, int y, int x, char dir)
 	else if (dir == 's' && map->map[y + 1][x] != '1')
 	{
 		if (map->map[y + 1][x] == 'C')
+		{
 			map->coin_c -= 1;
+			map->map[y + 1][x] = '0';
+		}
 		if (map->map[y + 1][x] == 'E' && map->coin_c == 0)
 			mlx_close_window(map->wind);
 		a_move_map_charge(map, y + 1, x);
@@ -39,7 +45,10 @@ static void	move_p_x(t_map *map, int y, int x, char dir)
 	if (dir == 'a' && map->map[y][x - 1] != '1')
 	{
 		if (map->map[y][x - 1] == 'C')
+		{
 			map->coin_c -= 1;
+			map->map[y][x - 1] = '0';
+		}
 		if (map->map[y][x - 1] == 'E' && map->coin_c == 0)
 			mlx_close_window(map->wind);
 		a_move_map_charge(map, y, x - 1);
@@ -48,7 +57,10 @@ static void	move_p_x(t_map *map, int y, int x, char dir)
 	else if (dir == 'd' && map->map[y][x + 1] != '1')
 	{
 		if (map->map[y][x + 1] == 'C')
+		{
 			map->coin_c -= 1;
+			map->map[y][x + 1] = '0';
+		}
 		if (map->map[y][x + 1] == 'E' && map->coin_c == 0)
 			mlx_close_window(map->wind);
 		a_move_map_charge(map, y, x + 1);
@@ -61,12 +73,6 @@ void	on_key_press(mlx_key_data_t keydata, void *param)
 	t_map	*map;
 
 	map = param;
-	/* int i = 0;
-	while (map->map[i])
-	{
-		printf("%s\n", map->map[i]);
-		i++;
-	} */
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
 		mlx_close_window(map->wind);
 	if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)
